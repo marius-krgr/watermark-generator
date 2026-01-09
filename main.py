ab2e20dd1754ea22c2bf3f6c7bb7b8fe57055b36
ellllllln <elleniminternetz@online.de>
2026-01-09 17:37:29 +0100

    Willkommen angefangen

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def main():
     # userinterface
 
     print("Hello, World!")
+    welcome()
+    queryInput()
     
 if __name__ == "__main__":
     main()
+
+
+def welcome():
+    print("Moin! Ich bin Watermarky, dein persönlicher Bildbearbeitungs- und Wassermarkierungs-Ersteller-Assistent :)")
+
+def queryInput():
+    print("Bitte lege deine Bilddateien im Ordner 'Import' ab.")
+    input("Um weiterzugehen, drücke Enter.")
