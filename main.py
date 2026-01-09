32288defe8d3ec4760bd847bd16d52ab0a215e1e
ellllllln <elleniminternetz@online.de>
2026-01-09 17:58:58 +0100

    added Import

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-# hello world
+import os
 
-def main():
-    # begrüße
-    # vorbereitung
-    # userinterface
 
-    print("Hello, World!")
-    welcome()
-    queryInput()
-    
-if __name__ == "__main__":
-    main()
-
+def fileImport(filename, folder):
+    filepath = os.path.join(folder, filename)
 
 def welcome():
     print("Moin! Ich bin Watermarky, dein persönlicher Bildbearbeitungs- und Wassermarkierungs-Ersteller-Assistent :)")
@@ -19,3 +10,29 @@ def welcome():
 def queryInput():
     print("Bitte lege deine Bilddateien im Ordner 'Import' ab.")
     input("Um weiterzugehen, drücke Enter.")
+
+    while True:
+        answer = input("Wie heißt die Datei, die du verwenden möchtest?\nEingabe: ")
+        if not answer.lower().endswith(".png"):
+            answer += ".png"
+        path = os.path.join("Import", answer)
+
+        if os.path.isfile(path):
+            return answer
+        else:
+            print("ACHTUNG: Datei '{answer}' wurde nicht gefunden!")
+            print("Bitte stelle sicher, dass die Datei im Ordner 'Import' liegt und überprüfe deine Schreibweise.\n")
+
+def main():
+    # begrüße
+    # vorbereitung
+    # userinterface
+    welcome()
+    filename=queryInput()
+    fileImport(filename, "/Import")
+
+
+    print("Hello, World!")
+    
+if __name__ == "__main__":
+    main()
